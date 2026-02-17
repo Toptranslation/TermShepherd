c51718191247fe7f0eed42652b0f0857c3fcf7ac
Farson <wg@toptranslation.com>
2026-02-17 14:48:44 +0100

    update hero-bg image

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,9 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ProductsComponent } from '../products/products.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
     selector: 'app-home',
-    imports: [ProductsComponent],
+    imports: [
+      NgOptimizedImage,
+      ProductsComponent
+    ],
     templateUrl: './home.html',
     styleUrl: './home.css',
     changeDetection: ChangeDetectionStrategy.OnPush
